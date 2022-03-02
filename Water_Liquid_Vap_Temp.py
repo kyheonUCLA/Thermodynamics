9145b72a175fb8e910e2638d43b7a8de87592443
kyco12 <86087539+kyco12@users.noreply.github.com>
2022-03-02 11:15:53 -0800

    Added interpolation functionality

--- Water_Liquid_Vap_Temp.py
+++ Water_Liquid_Vap_Temp.py
@@ -1,32 +1,60 @@
 import pandas as pd
 import numpy as np
-
+from icecream import ic
 #operate on the assumption that use temp value to get the rest of the properties
 #assume user has already verified that P and T values are at or above saturation
+
+#might be able to abstract this to also include pressure table
 class Water_Liquid_vap_Temp(object):
     def __init__(self, filename):
         self.data = pd.read_csv(filename)
+        self.MIN_T = self.data['T'].iat[0]
+        self.MAX_T = self.data['T'].iat[-1]
+        ic(self.MIN_T)
+        ic(self.MAX_T)
 
     def get_properties(self, T):
         temps = self.data['T']
-
         #change == later since dtype is float
+        # check if T < MaxT and T > minT
+
+        if T < self.MIN_T or T > self.MAX_T:
+            ic('T out of range')
+            return None
+
         lbound = None
         hbound = None
+        found = False
         for i in range(len(temps)):
             if temps[i] == T:
+                ic('Found')
                 return self.data.iloc[i] # returns column 
             elif temps[i] < T:
                 lbound = i
             elif temps[i] > T:
                 hbound = i
-        if lbound != hbound:
-            return self.lerp(lbound, hbound)
+                break #finding l and h bounds
+        if lbound < hbound:
+            ic('interp')
+            ic(lbound)
+            ic(hbound)
+            return self.interpolate(lbound, hbound, T)
+            
+        ic('Other')
         
 
-    def lerp(self, i1, i2):
-        df = {''} # need to interpolate and return df with computed values
+    def interpolate(self, i1, i2, x):  
         lrow = self.data.iloc[i1]
         hrow = self.data.iloc[i2]
-        for l, h in zip(lrow, hrow):
+        T1 = lrow['T']
+        T2 = hrow['T']
+        df = lrow.copy(deep=True)
+        lerp = lambda p1, p2, x : (p1[1]-p2[1]) / (p1[0]-p2[0]) * (x - p1[0]) + p1[1]
+        for key, value in df.iteritems():
+            if key == 'T':
+                df = df.replace({df[key] : x})
+            else:
+                y = lerp((T1, lrow[key]), (T2, hrow[key]), x)
+                df = df.replace({df[key] : y})
+        return df
 
--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-import pandas as pd
+from Water_Liquid_Vap_Temp import Water_Liquid_vap_Temp
+from icecream import ic
 
-data = pd.read_csv('data/csv_files/test1.csv')
+obj = Water_Liquid_vap_Temp('data/csv_files/test1.csv')
+
+#error whent T = 1*C
+ic(obj.get_properties(111))
+
